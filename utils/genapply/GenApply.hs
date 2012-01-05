bf9a4dbdbcc1282062d4c963902b95da681c7c81
Gabor Greif <ggreif@gmail.com>
2012-01-05 02:46:53 +0100

    use LANGUAGE pragma

--- utils/genapply/GenApply.hs
+++ utils/genapply/GenApply.hs
@@ -1,6 +1,6 @@
-{-# OPTIONS -cpp -fglasgow-exts #-}
+{-# LANGUAGE CPP #-}
 {-# OPTIONS -w #-}
--- The above warning supression flag is a temporary kludge.
+-- The above warning suppression flag is a temporary kludge.
 -- While working on this module you are encouraged to remove it and fix
 -- any warnings in the module. See
 --     http://hackage.haskell.org/trac/ghc/wiki/Commentary/CodingStyle#Warnings
